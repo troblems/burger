f3f3d168ab569079b4d0ad047ef31f5c5b7f07ea
Amanda Wittenstein <mashka.lewis@gmail.com>
2017-08-08 17:56:44 -0500

    update for connection, db, controllers

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Hawaiian Burger');
+INSERT INTO burgers(burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Lamburger');
+INSERT INTO burgers(burger_name) VALUES ('Chicken Jalapeño Burger');
