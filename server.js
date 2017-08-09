a13b3eedc23696ac990a18d7a71e44c63bdcfcf1
Amanda Wittenstein <mashka.lewis@gmail.com>
2017-08-09 09:17:02 -0500

    pathing error lol

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.engine('hbs', exphbs({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-var routes = require('./controllers/burgers_controllers.js');
+var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
 //starting server
